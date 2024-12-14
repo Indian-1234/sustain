548e00498ba9b696aa7135bbd7de6d76bc1c6e6d
Indian-1234 <indiangithub321@gmail.com>
2024-12-14 19:15:31 +0530

    backround color chage

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,11 +5,11 @@ import Navbar from "./nav.js";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="">
       <div className="mb-10 shadow-gray-400">
       <Navbar/>
       </div>
-    <div className="bg-gray-900 text-white">
+    <div className=" text-white">
       <div className=" w-16 fixed top-0 left-0 z-50">
         <Sidebar />
       </div>
